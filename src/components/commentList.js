75409f32672702b18bd82e262aa181f59c6f4736
enkdress <felizjanuk12@gmail.com>
2020-09-19 12:17:30 -0500

    changed the refresh logic to only the list

--- src/components/commentList.js
+++ src/components/commentList.js
@@ -1,18 +1,34 @@
-import { List, Comment } from 'antd'
+import useSWR from 'swr';
+import { List, Comment } from 'antd';
 
-export default function CommentList({ comments }) {
-  return (
-    <List
-      dataSource={comments}
-      header={`${comments.length} ${comments.length > 1 ? 'replies' : 'reply'}`}
-      itemLayout="horizontal"
-      renderItem={(props) => (
-        <Comment
-          avatar="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
-          content={<p>{props.comment}</p>}
-          author={props.username}
-        />
-      )}
-    />
-  );
+export default function CommentList() {
+  const { data, isValidating, error } = useSWR('http://localhost:8080/api/comments', { refreshInterval: 5000 });
+
+  if (!data && isValidating) return null;
+  if (error) return null;
+  if (data) {
+    const comments = data.comments;
+    const selectedComment = document.getElementById(`comment-${comments.length - 2}`)?.offsetTop;
+    const commentList = document.getElementById('list');
+    if (commentList) {
+      commentList.scrollTo(0, selectedComment);
+    }
+
+    return (
+      <List
+        dataSource={comments}
+        header={`${comments.length} ${comments.length > 1 ? 'replies' : 'reply'}`}
+        itemLayout="horizontal"
+        renderItem={(props) => (
+          <Comment
+            avatar="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+            content={<p>{props.comment}</p>}
+            author={props.username}
+            id={`comment-${comments.indexOf(props)}`}
+          />
+        )}
+      />
+    );
+  }
+  return null;
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
-import useSWR from 'swr';
 import { useState } from 'react';
 import AddComment from '../components/addComment';
 import CommentList from '../components/commentList';
@@ -6,24 +5,12 @@ import SignInModal from '../components/signin';
 
 export default function Index() {
   const [ haveToLogIn, setHaveToLogIn ] = useState(false);
-  const { data, isValidating, error } = useSWR('/api/comments');
 
-  if (isValidating || !data) return <p>Loading...</p>;
-  if (error) return <p>Error</p>;
-  if (data)
-    return (
-      <div>
-        {data.comments.length > 0 && <CommentList comments={data.comments || data} />}
-        <AddComment setHaveToLogIn={setHaveToLogIn} />
-        <SignInModal haveToLogIn={haveToLogIn} setHaveToLogIn={setHaveToLogIn} />
-      </div>
-    );
+  return (
+    <div>
+      <CommentList />
+      <AddComment setHaveToLogIn={setHaveToLogIn} />
+      <SignInModal haveToLogIn={haveToLogIn} setHaveToLogIn={setHaveToLogIn} />
+    </div>
+  );
 }
-
-// export async function getServerSideProps() {
-//   const response = await fetch('http://localhost:8080/api/comments');
-//   const comments = await response.json();
-//   return {
-//     props: { comments }
-//   };
-// }
